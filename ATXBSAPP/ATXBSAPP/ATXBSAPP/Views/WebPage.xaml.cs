b3936d8dba19dd64448960449f4bc03aa3760a7e
Cynthia Karina <40874570+CynthiaKarina@users.noreply.github.com>
2020-06-05 11:29:02 -0500

    WebView

--- ATXBSAPP/ATXBSAPP/ATXBSAPP/Views/WebPage.xaml.cs
+++ ATXBSAPP/ATXBSAPP/ATXBSAPP/Views/WebPage.xaml.cs
@@ -12,19 +12,14 @@ namespace ATXBSAPP.Views
         {
             Title = "ChatBot";
             InitializeComponent();
-            var browser = new WebView();
-            browser.Source = "https://atxbot.azurewebsites.net/bot.html";            
-                   
-            this.Content = browser;
+           
         }
-        private async void Back_Clicked(object sender, EventArgs e)
-        {
-            await Navigation.PopAsync();
-        }
+        //https://atxbot.azurewebsites.net/bot.html
 
         async void home_Clicked(object sender, EventArgs e)
         {
             await RootPage.NavigateFromMenu(0);
         }
+        
     }
 }
